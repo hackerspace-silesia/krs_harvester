74f8985b93fe08293f30bdb2f68a3489db9dd3ff
Firemark <marpiechula@gmail.com>
2016-09-24 14:12:11 +0200

    add models

--- backend/krs_harvester/donations/models.py
+++ backend/krs_harvester/donations/models.py
@@ -1,5 +1,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from organizations.models import Organization
 
-# Create your models here.
+
+class Donator(models.Model):
+    pk = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=150)
+
+
+class Donation(models.Model):
+    pk = models.UUIDField(primary_key=True)
+    donator = models.ForeignKey(Donator)
+    organization = models.ForeignKey(Organization)
+    name = models.CharField(max_length=150)
+    money = models.DecimalField(decimal_places=2)
+    date = models.DateField()
+    with_day = models.BooleanField(default=True)
+    with_month = models.BooleanField(default=True)
+
+    #todo: ?
--- backend/krs_harvester/organizations/models.py
+++ backend/krs_harvester/organizations/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Organization(models.Model):
+    pk = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=150)
+    krs = models.IntegerField()
+    address = models.CharField(max_length=300)
+    zip_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=200)
+    latitude = models.DecimalField(max_digits=8, decimal_places=5, null=True)
+    longitude = models.DecimalField(max_digits=8, decimal_places=5, null=True)
+    #todo: ?
